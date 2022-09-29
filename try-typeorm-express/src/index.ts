27483df34550a6c4efa02deaf8cccca4f8318cbd
= <j.mosavi2@gmail.com>
2022-09-29 17:00:25 +0330

    during local strategy

--- try-typeorm-express/src/index.ts
+++ try-typeorm-express/src/index.ts
@@ -2,15 +2,39 @@ import { AppDataSource } from "./data-source"
 import { Person } from "./entity/person"
 import express, {json} from "express"
 import {getAll, insertPerson } from "./services/people-service";
+import passport from "passport";
+import {Strategy as LocalStrategy} from 'passport-local'
+
 AppDataSource.initialize().then(async () => {
     const app = express();
     app.use(json());
+    app.use(passport.initialize());
+
+    passport.serializeUser(function(user, cb){
+        console.log(cb);
+    })
+
+    passport.use(new LocalStrategy(
+      {
+          usernameField: 'username',
+          passwordField: 'password',
+          passReqToCallback: true,
+      },(username, password, done) => {
+        if (!username || !password)
+            return done('ERROR: UNAUTHORIZED EXCEPTION')
+        return done(null, {name: 'Seyed Jafar Mousavi',},{message: 'ESKELENT, THE USER AUTHORIZED'});
+    }));
 
     app.get("/", async (req, res) =>{
         let result = await getAll();
         console.log('result is:' , result);
         return res.send(result);
     })
+
+    app.post("/login", passport.authenticate('local', { failureMessage: 'Authentication failed...static message'}, (req, res) =>{
+        console.log('authentication successful....');
+        return res.redirect("/");
+    }))
 
     app.post("/", async (req, res) =>{
         let result = await insertPerson(req.body);
